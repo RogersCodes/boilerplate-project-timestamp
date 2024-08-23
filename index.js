1154ab0dca6cffdd52cbd73004d02b98b1038e3a
RogersCodes <adindajohnrogers@gmail.com>
2024-08-23 11:20:49 +0300

    feat: updated code

--- index.js
+++ index.js
@@ -20,8 +20,35 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  let unixDate;
+  let dateObj;
+  let utcDate;
+  //Check whether i/o is a number using regex
+  let isUnix = /^\d+$/.test(date);
+
+  if(!date) {
+    dateObj = new Date();
+  } 
+  
+  else if (date && isUnix) {
+    unixDate = parseInt(date);
+    dateObj = new Date(unixDate);
+  }
+
+  //else build the date object and get the unix time
+  else if(date && !isUnix) {
+    dateObj = new Date(date);
+  }
+  if(dateObj.toUTCString() === "Invalid Date") {
+    res.json({error: "Invalid Date"})
+    return;
+  }
+  unixDate = dateObj.getTime();
+  utcDate = dateObj.toUTCString();
+
+  res.json({unix: unixDate, utc: utcDate});
 });
 
 
